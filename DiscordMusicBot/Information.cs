5656e6c1b06ae265764849bd128e42ae7431e95b
Marc Rousavy <Marc Rousavy>
2017-04-19 14:26:40 +0200

    Added Information.cs

---
+++ DiscordMusicBot/Information.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordMusicBot {
+    internal static class Information {
+        internal static string ClientId = "YourClientID";
+        internal static string ClientSecret = "YourClientSecret";
+        internal static string BotName = "TheBotsAreTakingOver";
+    }
+}
