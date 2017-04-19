9a5c395894dec0c74218141a31bd4d4cdaf19dfb
Marc Rousavy <Marc Rousavy>
2017-04-19 13:54:23 +0200

    Project Files added

---
+++ DiscordMusicBot/MusicBot.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordMusicBot {
+    internal class MusicBot {
+        public MusicBot() { }
+    }
+}
---
+++ DiscordMusicBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordMusicBot
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Initialized");
+        }
+    }
+}
