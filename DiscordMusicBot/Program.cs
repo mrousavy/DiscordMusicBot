8da49128ee2a2e03d29b4333ad0b435e0dbea95d
mrousavy <marcrousavy@hotmail.com>
2017-04-22 18:15:57 +0200

    txt to json

--- DiscordMusicBot/Program.cs
+++ DiscordMusicBot/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Diagnostics;
 using System.IO;
 
@@ -9,21 +10,26 @@ namespace DiscordMusicBot {
             Console.WriteLine("(Press Ctrl + C to exit Bot)");
 
             try {
-                //string json = File.ReadAllText("config.json");
-                //Config cfg = JsonConvert.DeserializeObject<Config>(json);
+                #region JSON.NET
+                string json = File.ReadAllText("config.json");
+                Config cfg = JsonConvert.DeserializeObject<Config>(json);
 
-                //if (cfg == new Config())
-                //    throw new Exception("Config is default Config!");
+                if (cfg == new Config())
+                    throw new Exception("Please insert values into Config.json!");
+                #endregion
 
-                string[] config = File.ReadAllLines("config.txt");
-                Config cfg = new Config() {
-                    BotName = config[0].Split(':')[1],
-                    ChannelName = config[1].Split(':')[1],
-                    ClientId = config[2].Split(':')[1],
-                    ClientSecret = config[3].Split(':')[1],
-                    ServerName = config[4].Split(':')[1],
-                    Token = config[5].Split(':')[1],
-                };
+
+                #region TXT Reading
+                //string[] config = File.ReadAllLines("config.txt");
+                //Config cfg = new Config() {
+                //    BotName = config[0].Split(':')[1],
+                //    ChannelName = config[1].Split(':')[1],
+                //    ClientId = config[2].Split(':')[1],
+                //    ClientSecret = config[3].Split(':')[1],
+                //    ServerName = config[4].Split(':')[1],
+                //    Token = config[5].Split(':')[1],
+                //};
+                #endregion
             } catch (Exception e) {
                 Console.WriteLine("Your config.json has incorrect formatting, or is not readable!");
                 Console.WriteLine(e.Message);
